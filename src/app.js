70c7f77306835f07b04ad31f8989a5c6a22a7866
Adgar <YOUR EMAIL@SAMLECOM>
2025-08-18 18:57:41 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const url = require('url');
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    const query = parsedUrl.query;
+
+    if ('hello' in query) {
+        if (query.hello && query.hello.trim() !== '') {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(`Hello, ${query.hello}.`);
+        } else {
+            response.writeHead(400, { 'Content-Type': 'text/plain' });
+            response.end('Enter a name');
+        }
+        return; 
+    }
+    
+    
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello world");
+});
+
+server.listen(3003, '127.0.0.1', () => {
+    console.log("Сервер запущен на http://127.0.0.1:3003");
+});
